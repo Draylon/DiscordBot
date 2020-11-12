517e40148346708f805eb0fac1e6ad4a72a5ba76
Draylon <draylid@gmail.com>
2020-11-12 11:03:07 -0300

    testing heroku

---
+++ commands/_tprog.js
@@ -0,0 +1,17 @@
+
+const ProgressBar = require('../utils/Progress');
+
+
+module.exports = async( client,message,args) => {
+    const pbar = new ProgressBar(':bar',{
+        curr:args[0],
+        incomplete:'_',
+        complete: ':',
+        head: '#',
+        width:55,
+        total: Math.max(args[1],1)
+    });
+    pbar.render();
+    message.delete();
+    message.channel.send("Percent: "+pbar.prcnt+" `"+pbar.lastDraw+"`");
+};
